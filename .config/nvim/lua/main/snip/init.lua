ad0b845b8b1fe37e3d3b81f946033538daf2d5d5
i-vrnv <ignatiy.voronov@gmail.com>
2022-11-27 11:32:53 +0100

    Fix snippets path

--- .config/nvim/lua/main/snip/init.lua
+++ .config/nvim/lua/main/snip/init.lua
@@ -1,6 +1,6 @@
 local M = {}
 
-local snippets_folder = vim.fn.stdpath "config" .. "/lua/config/snip/snippets/"
+local snippets_folder = vim.fn.stdpath "config" .. "/lua/main/snip/snippets/"
 local ls = require("luasnip")
 
 local types = require "luasnip.util.types"
@@ -25,16 +25,9 @@ function M.setup()
     },
   }
 
-
   require("luasnip.loaders.from_vscode").lazy_load()
   require("luasnip.loaders.from_snipmate").lazy_load()
   require("luasnip.loaders.from_lua").lazy_load { paths = snippets_folder }
-
-  -- shortcut for quick snippet file edit
-  -- vim.cmd [[command! LuaSnipEdit :lua require("luasnip.loaders.from_lua").edit_snippet_files()]]
-
-  -- require("luasnip.loaders.from_vscode").lazy_load { paths = { "./snippets/python" } }
-  -- require("luasnip.loaders.from_vscode").lazy_load { paths = { "./snippets/rust" } }
 
   ls.filetype_extend("all", { "_" })
 end
