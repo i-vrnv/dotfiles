28ac06e98461d8c2a90d47f4a8a4da7ace2f28cc
i-vrnv <ignatiy.voronov@gmail.com>
2023-01-12 15:22:28 +0100

    Neovim: Keybindings for LSP and local clipboard

--- .config/nvim/lua/main/lsp/keymaps.lua
+++ .config/nvim/lua/main/lsp/keymaps.lua
@@ -4,7 +4,7 @@ M.setup = function(_, bufnr)
   local opts = { buffer = bufnr, remap = false }
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "gt", function() vim.lsp.buf.type_definition() end, opts)
+  vim.keymap.set("n", "gD", function() vim.lsp.buf.type_definition() end, opts)
   vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
   vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
