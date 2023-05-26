daf98e9aaad60b7ff79d1ff1978ba57e9b8bb0fa
i-vrnv <ignatiy.voronov@gmail.com>
2023-05-26 22:01:35 +0200

    Nvim: Add typescript support

--- .config/nvim/lua/main/treesitter.lua
+++ .config/nvim/lua/main/treesitter.lua
@@ -2,13 +2,13 @@ local M = {}
 
 function M.setup()
   require("nvim-treesitter.configs").setup {
-    ensure_installed = {"bash"," go", "java", "php", "phpdoc", "lua", "rust"},
+    ensure_installed = {"bash"," go", "java", "php", "phpdoc", "lua", "rust", "javascript", "typescript"},
     sync_install = false,
+    auto_install = true,
     highlight = {
       enable = true,
       additional_vim_regex_highlighting = false,
     },
-    indent = { enable = true, disable = { "python", "java", "rust", "lua" } },
   }
 end
 
