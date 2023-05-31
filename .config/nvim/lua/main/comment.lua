b648f7e48d938581cd27dc93767b35b1a22561ad
i-vrnv <ignatiy.voronov@gmail.com>
2023-05-31 22:22:41 +0200

    Nvim: add missing comment.lua

---
+++ .config/nvim/lua/main/comment.lua
@@ -0,0 +1,19 @@
+local M = {}
+
+function M.setup()
+  require('mini.comment').setup {
+    mappings = {
+      -- Toggle comment (like `gcip` - comment inner paragraph) for both
+      -- Normal and Visual modes
+      comment = '<C-_>', -- it's actually <C-/>  
+
+      -- Toggle comment on current line
+      comment_line = '',
+
+      -- Define 'comment' textobject (like `dgc` - delete whole comment block)
+      textobject = '<C-_>',
+    },
+  }
+end
+
+return M
