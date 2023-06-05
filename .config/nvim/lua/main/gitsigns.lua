f47c53cee973cf8fd884ec8395f21d542b8afe0a
i-vrnv <ignatiy.voronov@gmail.com>
2023-06-05 23:02:07 +0200

    Nvim: Use default gitsigns icons

--- .config/nvim/lua/main/gitsigns.lua
+++ .config/nvim/lua/main/gitsigns.lua
@@ -2,33 +2,6 @@ local M = {}
 
 function M.setup()
   require("gitsigns").setup {
-    signs = {
-      add = { hl = "GitSignsAdd", text = "▍", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-      change = {
-        hl = "GitSignsChange",
-        text = "▍",
-        numhl = "GitSignsChangeNr",
-        linehl = "GitSignsChangeLn",
-      },
-      delete = {
-        hl = "GitSignsDelete",
-        text = "▸",
-        numhl = "GitSignsDeleteNr",
-        linehl = "GitSignsDeleteLn",
-      },
-      topdelete = {
-        hl = "GitSignsDelete",
-        text = "▾",
-        numhl = "GitSignsDeleteNr",
-        linehl = "GitSignsDeleteLn",
-      },
-      changedelete = {
-        hl = "GitSignsChange",
-        text = "▍",
-        numhl = "GitSignsChangeNr",
-        linehl = "GitSignsChangeLn",
-      },
-    },
     keymaps = {
       -- Default keymap options
       noremap = true,
