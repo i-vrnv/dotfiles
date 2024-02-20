663c4daf4c0a434d6d921c6631c177380e6481f3
i-vrnv <ignatiy.voronov@gmail.com>
2024-02-20 22:35:12 +0100

    Nvim: Improve telescope hotkeys

--- .config/nvim/lua/main/telescope.lua
+++ .config/nvim/lua/main/telescope.lua
@@ -1,5 +1,18 @@
 local M = {}
 
+local function get_visual_selection()
+	vim.cmd('noau normal! "vy"')
+	local text = vim.fn.getreg('v')
+	vim.fn.setreg('v', {})
+
+	text = string.gsub(text, "\n", "")
+	if #text > 0 then
+		return text
+	else
+		return ''
+	end
+end
+
 function M.setup()
   local builtin = require('telescope.builtin')
 
@@ -11,10 +24,20 @@ function M.setup()
     builtin.find_files({ hidden = true })
   end, {})
 
-  vim.keymap.set("n", "<Leader>fs", builtin.live_grep, {})
+  vim.keymap.set("n", "<Leader>fs", function()
+    builtin.live_grep({ hidden = true })
+  end, {})
+
   vim.keymap.set("n", "<Leader>fg", builtin.git_files, {})
   vim.keymap.set("n", "<Leader>fb", builtin.buffers, {})
-  vim.keymap.set('n', '<leader>ps', function() builtin.grep_string({ search = vim.fn.input("Grep > ") }) end)
+
+  vim.keymap.set('n', '<leader>pg', function()
+    builtin.grep_string({ search = vim.fn.input("Grep > ") })
+  end)
+
+  vim.keymap.set("v", "<Leader>pg", function()
+    builtin.grep_string({ search = get_visual_selection() })
+  end, {})
 
   vim.keymap.set("n", "<Leader>vc", builtin.git_commits, {})
   vim.keymap.set("n", "<Leader>vb", builtin.git_branches, {})
@@ -94,7 +117,6 @@ function M.setup()
 
   require('telescope').load_extension('fzf')
   require("telescope").load_extension("ui-select")
-
 end
 
 return M
