d18ad5eff1cd55fe91da24edeb4a3ad1330d91ea
i-vrnv <ignatiy.voronov@gmail.com>
2024-05-27 10:19:19 +0200

    Nvim: Lualine catppuccin

--- .config/nvim/lua/main/plugins/lualine.lua
+++ .config/nvim/lua/main/plugins/lualine.lua
@@ -5,7 +5,7 @@ return {
   config = function()
     require('lualine').setup {
       options = {
---        theme = 'tokyonight',
+        theme = "catppuccin-mocha",
         section_separators = '',
         component_separators = ''
       }
